aeb28754e1a9de1f966a2a508bcb7162c2a37bbb
133931212-myseneca <133059127+133931212-myseneca@users.noreply.github.com>
2023-06-13 20:17:04 -0400

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,1 +1,10 @@
+def write_text_file_with_name(name):
+    filename = "output.txt"
+    with open(filename, "w") as file:
+        file.write("My name is: " + name)
+    print("Text file '{}' created successfully!".format(filename))
+
+# Example usage:
+name = input("Enter your name: ")
+write_text_file_with_name(name)
 
